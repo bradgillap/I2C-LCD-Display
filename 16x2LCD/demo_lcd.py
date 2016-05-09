856a347c24038a0e953bf44f9a9708a08d21ade7
Bradley Gillap <bradgillap@gmail.com>
2016-05-08 22:04:30 -0400

    Update demo_lcd.py

--- 16x2LCD/demo_lcd.py
+++ 16x2LCD/demo_lcd.py
@@ -15,10 +15,11 @@ try:
     while True:
         # Remember that your sentences can only be 16 characters long!
         print("Writing to display")
-        display.lcd_display_string("Hey jerk, wanna", 1) # Write line of text to first line of display
+        display.lcd_display_string("Hey jerk, wanna", 1)  # Write line of text to first line of display
         display.lcd_display_string("kill all humans?", 2) # Write line of text to second line of display
         time.sleep(2)                                     # Give time for the message to be read
-        display.lcd_display_string("Insert Liquor", 1)  # Refresh the first line of display with a different message
+        display.lcd_display_string("Insert Liquor", 1)    # Refresh the first line of display with a different message
+        display.lcd_display_string("", 2)                 # Write line of text to second line of display
         time.sleep(2)                                     # Give time for the message to be read
         display.lcd_clear()                               # Clear the display of any data
         time.sleep(2)                                     # Give time for the message to be read
